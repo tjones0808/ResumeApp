cfa4797a7e4798ed63b1e2b84ae88dd3393e1a1a
tjones0808 <tylerjones81@gmail.com>
2015-08-20 10:55:05 -0400

    Revert "initial commit"
    This reverts commit 7d0ffb44ef3e366ac15c858eb5dfff1fe5b28c0b.

--- contact_form/contact_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "tylerjones81@gmail.com";
-$name = $_POST['name'];
-$message = $_POST['message'];
-$from = $_POST['email'];
-$headers = "From:" . $from;
-mail($to,$name,$message,$headers);
-echo "Mail Sent.";
-?>
